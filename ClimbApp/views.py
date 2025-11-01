cfa32a02bc9da376fc2eefa05ac2d3553b636dc2
ethan-ignatius <ethanignatius111@gmail.com>
2025-11-01 00:15:44 -0400

    map landing page

--- ClimbApp/views.py
+++ ClimbApp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from routes.models import Route
 
 def home(request):
-    return render(request, "home.html")
+    # Get all routes that have coordinates for the map
+    routes_with_coords = Route.objects.filter(
+        latitude__isnull=False, 
+        longitude__isnull=False
+    ).select_related('author')
+    
+    return render(request, "home.html", {"routes": routes_with_coords})
